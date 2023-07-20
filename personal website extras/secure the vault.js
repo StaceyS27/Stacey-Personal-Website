dbc84a2915a9eb0b9af0d7fef541231dd1c4a26e
Stacey Sanchez <staceysanchez@macbook-air.mynetworksettings.com>
2023-07-20 11:41:16 -0400

    links and photos used on website

---
+++ personal website extras/secure the vault.js
@@ -0,0 +1,16 @@
+/*  1. set variables where the arithmetic equals each combination
+    2. set a variable for the string "You have received this message because you have been chosen to open an important vault. 
+    Here is the secret combination:"
+    3. create dialog box by using alert function
+    4. concatenate string and code variables in alert function
+    5. link js file in html
+*/
+   
+let code1 = 20-10;     //10
+let code2 = 30+10;     //40
+let code3 = 13*3;      //39
+
+let string = "You have received this message because you have been chosen to open an important vault. Here is the secret combination: ";
+
+
+alert (string + code1 + " - " + code2 + " - " + code3);
